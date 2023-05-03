1cf3b0e5cbc4ee29fe9d8522ac629f9220ad0412
DAZUMYT <rosa7casa@gmail.com>
2023-05-03 10:38:03 +0200

    NetBomb v0.5: Added ssh

--- NetBomb.py
+++ NetBomb.py
@@ -23,13 +23,17 @@ white="\033[0;37m"
 nc="\033[00m"
 ##
 
-## LISTS
+## VARIABLES
 tryPorts = [21,22,80,194,443,8080,445,389,88,135,515,631,1080,3306,3389,9100]
 openPorts = []
-printer = []
+
 activeHosts = []
 menu = ""
 ##
+
+## TYPES OF HOSTS
+printerHost = []
+sshHost = []
 
 def portScanner(): # The main scan function
     networkIp = input(f"\nPlease enter your range of {byellow}IP{nc} (for example, '192.168.0')\n\n{bblue}[NetBomb] {bcyan}>>{bred} ")
@@ -55,34 +59,51 @@ def portScanner(): # The main scan function
                 
                 match port:
                     case 515:
-                        if host in printer:
+                        if host in printerHost:
                             pass
                         else:
-                            printer.append(host)
+                            printerHost.append(host)
+                    
                     case 631:    
-                        if host in printer:
+                        if host in printerHost:
                             pass
                         else:
-                            printer.append(host)
+                            printerHost.append(host)
+                    
                     case 9100:    
-                        if host in printer:
+                        if host in printerHost:
                             pass
                         else:
-                            printer.append(host)
+                            printerHost.append(host)
+
+                    case 22:    
+                        if host in sshHost:
+                            pass
+                        else:
+                            sshHost.append(host)
             sock.close()
     typeCounter(menu)
     
 def typeCounter(menu):
     menu = f"\n\n{byellow}[RESTART]{nc} restart port scanner\n\nThere are {len(activeHosts)} {bgreen}connected{nc} hosts\n\n"
     
-    if len(printer) == 1:
+    if len(printerHost) == 1:
         menu += "There is a printer\n\n"
 
     else:
-        menu += f"There are {len(printer)} printers\n\n"
+        menu += f"There are {len(printerHost)} printers\n\n"
     
-    if printer:
+    if printerHost:
         menu += f"{byellow}[PR]{nc} Connect to printers\n"
+
+    if len(sshHost) == 1:
+        menu += "There is a ssh host\n\n"
+
+    else:
+        menu += f"There are {len(sshHost)} ssh hosts\n\n"
+    
+    if sshHost:
+        menu += f"{byellow}[SH]{nc} Connect to ssh hosts\n"
 
     menuSelector(menu)
 
@@ -111,23 +132,43 @@ def menuSelector(menu):
             print(f"\n{bblue}[NetBomb] {bgreen}Restarting Port Scanner....{nc}")
             portScanner()
         
+        case "SH":
+            print(f"\n{bblue}[NetBomb] {nc}Connecting to {bgreen}ssh hosts{nc}....")
+            sshScanner()
+        
+        case "sh":
+            print(f"\n{bblue}[NetBomb] {nc}Connecting to {bgreen}hosts{nc}....")
+            sshScanner()
+
         case _:
             print("select an option")
             menuSelector(menu)
 
 def printerScanner():
     menu = ""
-    if len(printer) == 0:
+    if len(printerHost) == 0:
         print(f"\n\n{bred}[!] There is not an active printer in the network.{nc}")
 
-    for printers in printer:
+    for printers in printerHost:
         print(printers)
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.connect((printers, 9100))
         sock.sendall(b"@PJL INFO ID\n")
         data = sock.recv(1024).decode("utf-8")
-        print("\n\nCONNECTION ACCEPTED\n\n",printer, f" {byellow}PRINTER TYPE: {red}", data, f"{nc}")
+        print("\n\nCONNECTION ACCEPTED\n\n",printerHost, f" {byellow}PRINTER TYPE: {red}", data, f"{nc}")
         menu += f" {byellow}PRINTER TYPE: {red}" + data + f"{nc}"
+    
+    menuSelector(menu)
+
+def sshScanner():
+    menu = ""
+    if len(sshHost) == 0:
+        print(f"\n\n{bred}[!] There is not an active ssh host in the network.{nc}")
+
+    for sHost in sshHost:
+        print(f"{byellow}SSH{nc} HOST: ", sHost)
+
+
     
     menuSelector(menu)
 
